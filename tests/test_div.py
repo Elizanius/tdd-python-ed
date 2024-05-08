0b065c7310ffa25d0af199c6bab7624905dd0d42
elizanius <izanhc19@gmail.com>
2024-05-08 16:27:10 +0200

    arreglando errores

--- tests/test_div.py
+++ tests/test_div.py
@@ -5,7 +5,7 @@ from ejercicios.operaciones import div
 class DivTestClass:
 
     def test_div(self):
-        assert div(2,1) == 1
-        assert div(-1,0) == 0
-        assert div(4,2) == 2
-        assert div(-2,-1) == -2
+        assert div(2,1) == 2.0
+        assert div(-1,0) == 0.0
+        assert div(4,2) == 2.0
+        assert div(-2,-1) == 2.0
