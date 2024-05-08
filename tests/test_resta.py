540f0603dce535dfe82496870f17dff9e0755b80
elizanius <izanhc19@gmail.com>
2024-05-08 16:37:23 +0200

    Arreglando pt4

--- tests/test_resta.py
+++ tests/test_resta.py
@@ -7,5 +7,5 @@ class SumaTestClass:
     def test_resta(self):
         assert resta(9,5) == 4
         assert resta(-1,-2) == 1
-        assert resta(-7,5) == -9
+        assert resta(-7,5) == -12
         assert resta(-7,9) == -16
