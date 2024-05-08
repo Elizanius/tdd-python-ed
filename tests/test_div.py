122f16623652e804816246bdac8db599d3c20df8
elizanius <izanhc19@gmail.com>
2024-05-08 16:35:48 +0200

    Arreglando pt3

--- tests/test_div.py
+++ tests/test_div.py
@@ -6,6 +6,6 @@ class DivTestClass:
 
     def test_div(self):
         assert div(2,1) == 2.0
-        assert div(-1,0) == 0.0
+        assert div(1,1) == 1.0
         assert div(4,2) == 2.0
         assert div(-2,1) == -2.0
